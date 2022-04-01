7efaf8beefad5e2eb0355e3f0ee9ce0c4a9b1280
Alice Hsing <alicehsing@gmail.com>
2022-04-01 12:50:26 -0700

    added create method to UserService.js to create a user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -16,16 +16,16 @@ describe('backend-gitty routes', () => {
   });
 
   it('should redirect to the github oauth page upon login', async () => {
-    const req = await request(app).get('/api/v1/github/login');
+    const res = await request(app).get('/api/v1/github/login');
   
-    expect(req.header.location).toMatch(
+    expect(res.header.location).toMatch(
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
     );
   });
   
 
   it('should login and redirect users to /api/v1/posts', async () => {
-    const req = await request
+    const res = await request
       .agent(app)
       .get('/api/v1/github/login/callback?code=42')
       .redirects(1);
