52c2476fd8ae8c19f27569b3f48c9695a5ee13fb
Alice Hsing <alicehsing@gmail.com>
2022-04-01 10:05:12 -0700

    passed test for GET route /api/v1/github/login

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,11 +4,19 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('backend-gitty routes', () => {
-  beforeEach(() => {
-    return setup(pool);
-  });
+  // beforeEach(() => {
+  //   return setup(pool);
+  // });
 
-  afterAll(() => {
-    pool.end();
-  });
+  // afterAll(() => {
+  //   pool.end();
+  // });
 });
+
+it('should redirect to the github oauth page upon login', async () => {
+  const req = await request(app).get('/api/v1/github/login');
+
+  expect(req.header.location).toMatch(
+    /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
+  );
+})
