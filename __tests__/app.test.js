239399153dfcaa21382560214a90f984c062dba4
Alice Hsing <alicehsing@gmail.com>
2022-04-01 11:58:24 -0700

    added front end UI in the Public folder

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,7 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 
-jest.mock('../lib/utils/github');
+// jest.mock('../lib/utils/github');
 
 describe('backend-gitty routes', () => {
   beforeEach(() => {
@@ -14,12 +14,24 @@ describe('backend-gitty routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+  it('should redirect to the github oauth page upon login', async () => {
+    const req = await request(app).get('/api/v1/github/login');
+  
+    expect(req.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
+    );
+  });
+  
+
+  it('should login and redirect users to /api/v1/posts', async () => {
+    const req = await request
+      .agent(app)
+      .get('/api/v1/github/login/callback?code=42')
+      .redirects(1);
+  
+      expect(res.req.path).toEqual('/api/v1/posts');
+  });
+
 });
 
-it('should redirect to the github oauth page upon login', async () => {
-  const req = await request(app).get('/api/v1/github/login');
-
-  expect(req.header.location).toMatch(
-    /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
-  );
-})
