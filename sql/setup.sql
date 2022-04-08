96f5ffe2561c1011a711bd03d2c522e41c32555c
Alice Hsing <alicehsing@gmail.com>
2022-04-07 17:53:56 -0700

    added /api/v1/quotes route, controller, model and sql table quotes

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,6 +2,7 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS github_users CASCADE;
 DROP TABLE IF EXISTS posts CASCADE;
+DROP TABLE IF EXISTS quotes CASCADE;
 
 
 CREATE TABLE github_users (
@@ -13,4 +14,10 @@ CREATE TABLE posts (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     text VARCHAR(255) NOT NULL,
     username TEXT REFERENCES github_users(github_username)
+);
+
+CREATE TABLE quotes (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    author TEXT NOT NULL,
+    content TEXT NOT NULL
 );
